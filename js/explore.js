6a3f7f4fc1684eea1ae1e505e0464d7a72b147fd
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2022-11-19 14:30:35 -0500

    layout and hover fixes

--- js/explore.js
+++ js/explore.js
@@ -19,6 +19,12 @@ function getRatingHTML(rating) {
 
     return stars;
 }
+class PriceCondElement {
+    constructor(price, condition) {
+        this.price = price;
+        this.condition = condition;
+    }
+}
 // class ItemListElement {
 //     constructor(image, title, rating) {
 //         this.title = title;
@@ -39,15 +45,23 @@ function getRatingHTML(rating) {
 //     }
 // }
 // class BookListElement extends ItemListElement {
-    class BookListElement {
-    constructor(image, title, rating, author) {
+class BookListElement {
+    constructor(image, title, rating, author, isbn, edition, price_cond, courseList) {
         // super(image, title, rating);
         this.author = author;
         this.title = title;
         this.image = image;
         this.rating = rating;
-    }
+        this.isbn = isbn; 
+        this.edition = edition; 
+        this.price_cond = price_cond; 
+        this.courseList = courseList;
+        }
     getHTML() {
+        var courseListHTML = "";
+        for(var i = 0; i < this.courseList.length; i++) {
+            courseListHTML += `<li>${this.courseList[i]}</li>\n`;
+        }
         return `<div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-xs-sm-6 py-4 explore-item-parent">
                     <div class="explore-item p-4 item-details">
                         <img src="${this.image}" alt="" class="explore-item-image">
@@ -56,17 +70,33 @@ function getRatingHTML(rating) {
                         <div class="stars">
                             ${getRatingHTML(this.rating)}
                         </div>
-                        <button class="explore-item-button">Add to cart</button>
+                        <button class="explore-item-button add-to-cart">Add to cart</button>
                     </div>
                     <div class="hide-item-details">
-                            <img src="${this.image}" alt="">
+                        <img src="${this.image}" alt="">
+                        <div class="item-text-details">
                             <h1>${this.title}</h1>
-                            <h3>${this.author}</h3>
+                            <h3>by ${this.author}</h3>
+                            <h3>ISBN: ${this.isbn}, Volume ${this.edition}</h3>
+                            <h3>From $${this.price_cond[0].price}, ${this.price_cond[0].condition} Condition</h3>
+                            <h5>Description:</h5>
+                            <p>Volume ${this.edition} covers a wide variety of key concepts under the topic of ${this.title.substr(0, this.title.indexOf(" "))}. The author, ${this.author}, has contributed much to the field and has shared their experiences throughout this book.</p>
+                            <div style="display:flex">
+                                <h4>Used in:</h4>
+                                <ul>
+                                    ${courseListHTML}
+                                </ul>
+                            </div>
                             <div class="stars">
                                 ${getRatingHTML(this.rating)}
                             </div>
-                            <button class="explore-item-button">Add to cart</button>
-                        Hello World
+                            <div class="explore-item-button-wrapper">
+                                <button class="explore-item-button add-to-cart">Add to cart</button>
+                            </div>
+                            <div class="explore-item-button-wrapper">
+                                <button class="explore-item-button add-to-wishlist">Add to wishlist</button>
+                            </div>
+                        </div>
                     </div>
                 </div>`
                 
@@ -85,22 +115,9 @@ for (var i = 0; i < 10; i++) {
         }
     }
 }
-bookCovers = ["../image/book-1.png",
-    "../image/book-2.png",
-    "../image/book-3.png",
-    "../image/book-4.png",
-    "../image/book-5.png",
-    "../image/book-6.png",
-    "../image/book-7.png",
-    "../image/book-8.png",
-    "../image/book-9.png",
-    "../image/book-10.png",
-    "../image/book3.png",
-    "../image/book5.png",
-    "../image/book7.png",
-]
+bookCovers = ["../image/book-1.png", "../image/book-2.png", "../image/book-3.png", "../image/book-4.png", "../image/book-5.png", "../image/book-6.png", "../image/book-7.png", "../image/book-8.png", "../image/book-9.png", "../image/book-10.png", "../image/book3.png", "../image/book5.png", "../image/book7.png"];
 surnames = [ "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Rodriguez", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young", "Hernandez", "King", "Wright", "Lopez", "Hill", "Scott", "Green", "Adams", "Baker", "Gonzalez", "Nelson", "Carter", "Mitchell", "Perez", "Roberts", "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins", "Stewart", "Sanchez", "Morris", "Rogers", "Reed", "Cook", "Morgan", "Bell", "Murphy", "Bailey", "Rivera", "Cooper", "Richardson", "Cox", "Howard", "Ward", "Torres", "Peterson", "Gray", "Ramirez", "James", "Watson", "Brooks", "Kelly", "Sanders", "Price", "Bennett", "Wood", "Barnes", "Ross", "Henderson", "Coleman", "Jenkins", "Perry", "Powell", "Long", "Patterson", "Hughes", "Flores", "Washington", "Butler", "Simmons", "Foster", "Gonzales", "Bryant", "Alexander", "Russell", "Griffin", "Diaz", "Hayes"];
-
+conditions = ["New","Excellent", "Good", "Fair", "Poor"];
 
 var itemList = []
 for (var i = 0; i < 300; i++) {
@@ -110,7 +127,18 @@ for (var i = 0; i < 300; i++) {
     surname = surnames[Math.floor(Math.random() * surnames.length)];
     firstInitial = letters[Math.floor(Math.random() * letters.length)];
     rating = Math.random() * 5;
-    itemList.push(new BookListElement(cover, `${courseName} ${courseNum}`, rating, `${firstInitial}. ${surname}`));
+    isbn = codes[Math.floor(Math.random() * codes.length)] + codes[Math.floor(Math.random() * codes.length)] + codes[Math.floor(Math.random() * codes.length)];
+    edition = Math.floor(Math.random() * 10);
+    courseListBooks = []
+    courseListBooks.push(`${courseName} ${courseNum}`);
+    for (var j = 0; j < 2; j++) {
+        courseListBooks.push(`${courses[Math.floor(Math.random() * courses.length)]} ${codes[Math.floor(Math.random() * codes.length)]}`);
+    }
+    price_cond = []
+    for (var j = 0; j < 20; j++) {
+        price_cond.push(new PriceCondElement(Math.floor(Math.random()*4000 + 1000)/100,conditions[Math.floor(Math.random()*conditions.length)]));
+    }
+    itemList.push(new BookListElement(cover, `${courseName} ${edition}`, rating, `${firstInitial}. ${surname}`, isbn, edition, price_cond, courseListBooks));
 }
 
 
@@ -135,19 +163,74 @@ function updateItemListingSortedByRating(direction, minRating, numOfItems) {
     }
     for (var i = 0; i < revisedItemList.length; i++) {
         if (i < numOfItems) {
-
+            
             $("#itemTemplate").append(revisedItemList[i].getHTML());
         } else { break; }
     }
+    var prevMouseEvent;
+    $(".hide-item-details").mouseenter(function(e) {
+        prevMouseEvent = e;
+    })
+    // $(".hide-item-details:offscreen").mouseenter(function(e) {
+    //     rect = e.getBoundingClientRect();
+    //     $(".hide-item-details").css('left', e.pageX - document.documentElement.scrollLeft );
+    //     $(".hide-item-details").css('top', e.pageY - document.documentElement.scrollTop );
+    // })
+
     $(".explore-item-image").mouseenter(function(e){
+        if (prevMouseEvent != null) {return;}
         e.preventDefault();
         console.log(e.pageX, e.pageY);
-        $(".hide-item-details").css('left', e.pageX );
-        $(".hide-item-details").css('top', e.pageY );
-        $(this).parent().next().css('display', 'block');
+        rect = document.documentElement.getBoundingClientRect();
+        console.log(rect);
+
+        //Width() here doesnt seem to be returning the proper size - we could instead just
+        //hardcode it here using 60% of the `rect` values...
+        //outerwidth doesnt help at all here
+        //
+        //but, using width() does give the correct offset, when the width actually triggers properly in the if statement
+        //we could try using `right` if the mouse cursor is past the halfway point...
+        //because the width of the box is 60% according to the css stuff anyways, so the middle might always get cut off?s
+        var xPos = e.pageX - document.documentElement.scrollLeft;
+        console.log(xPos);
+        console.log($(".hide-item-details").outerWidth(true));
+        console.log($(".hide-item-details").css("width"));
+        //right half
+        if (xPos >= 2*rect.right/3) {
+            console.log()
+            $(".hide-item-details").css('left', '' );
+            $(".hide-item-details").css('right', rect.right - xPos);
+
+        }
+        //center
+        else if(xPos > 3*rect.right/8 &&  xPos < 2* rect.right /3) {
+            $(".hide-item-details").css('right', '' );
+            $(".hide-item-details").css('left', xPos - $(".hide-item-details").width()/2 );
+
+        }
+        //left half
+        else {
+            $(".hide-item-details").css('right', '' );
+            $(".hide-item-details").css('left', xPos );
+        }
+
+        var yPos = e.pageY - document.documentElement.scrollTop;
+        
+        if (e.clientY + $(".hide-item-details").height() > window.innerHeight) {
+            $(".hide-item-details").css('top', '' );
+            $(".hide-item-details").css('bottom', window.innerHeight - e.clientY  );
+        }
+        else {
+            $(".hide-item-details").css('bottom', '' );
+            $(".hide-item-details").css('top', yPos );
+        }
+        // $(".hide-item-details").css('left', xPos );
+        $(this).parent().next().css('display', 'flex');
+
     });
     $(".explore-item-parent").mouseleave(function(e){
         $(this).children().eq(1).css('display', 'none');
+        prevMouseEvent = null;
     });
 
 }
