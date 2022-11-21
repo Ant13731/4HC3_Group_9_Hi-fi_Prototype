e35297d910917bfc596cb7622ce1e38655fee477
Ant13731 <69334555+Ant13731@users.noreply.github.com>
2022-11-21 16:00:17 -0500

    fixing links

--- js/itemDetails.js
+++ js/itemDetails.js
@@ -31,6 +31,9 @@ window.onload = ( () => {
 });
 
 $(document).ready(function() {
+    item =  JSON.parse( localStorage.getItem("itemDetails")) ?? fakeBook;
+    cartList = JSON.parse(localStorage.getItem('cartList')) ?? [];
+    wishList = JSON.parse(localStorage.getItem('wishList')) ?? [];
     $("#item-container").append(getHTML());
     $(`#cartID`).click(function (e) {
         if (!cartList.includes(item)) {
--- js/viewHeader.js
+++ js/viewHeader.js
@@ -14,32 +14,32 @@ document.querySelector("#search-btn").onclick = () => {
 	searchForm.classList.toggle("active");
 };
 
-let loginForm = document.querySelector(".login-form-container");
+// let loginForm = document.querySelector(".login-form-container");
 
-document.querySelector("#login-btn").onclick = () => {
-	loginForm.classList.toggle("active");
-};
+// document.querySelector("#login-btn").onclick = () => {
+// 	loginForm.classList.toggle("active");
+// };
 
-document.querySelector("#close-login-btn").onclick = () => {
-	loginForm.classList.remove("active");
-};
+// document.querySelector("#close-login-btn").onclick = () => {
+// 	loginForm.classList.remove("active");
+// };
 
-window.onscroll = () => {
-	searchForm.classList.remove("active");
+// window.onscroll = () => {
+// 	searchForm.classList.remove("active");
 
-	if (window.scrollY > 80) {
-		document.querySelector(".header .header-2").classList.add("active");
-	} else {
-		document.querySelector(".header .header-2").classList.remove("active");
-	}
-};
+// 	if (window.scrollY > 80) {
+// 		document.querySelector(".header .header-2").classList.add("active");
+// 	} else {
+// 		document.querySelector(".header .header-2").classList.remove("active");
+// 	}
+// };
 
-window.onload = () => {
-	if (window.scrollY > 80) {
-		document.querySelector(".header .header-2").classList.add("active");
-	} else {
-		document.querySelector(".header .header-2").classList.remove("active");
-	}
+// window.onload = () => {
+// 	if (window.scrollY > 80) {
+// 		document.querySelector(".header .header-2").classList.add("active");
+// 	} else {
+// 		document.querySelector(".header .header-2").classList.remove("active");
+// 	}
 
-	fadeOut();
-};
+// 	fadeOut();
+// };
