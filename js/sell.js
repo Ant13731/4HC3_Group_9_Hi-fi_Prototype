c74d4aff4e401c56d37f5b4d68f7034bed371c83
Aniruddh Arora <arora@macArora.local>
2022-11-27 19:25:48 -0500

    made new js file for sell

---
+++ js/sell.js
@@ -0,0 +1,12 @@
+function submit(){
+    console.log(this.document);
+    alert("Your item has been added to the cart!");
+}
+
+function saveForLater(){
+    alert("Your item has been saved for later!");
+}
+
+function remove(){
+    alert("Your item has been removed from the cart!");
+}
